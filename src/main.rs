d3a1638a5d4aa87812bbf41f0cf75f42b360bafb
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-04-11 20:36:27 +0200

    Characters

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,105 @@
-extern crate rand;
 extern crate sdl2;
 extern crate time;
 
+use std::path::Path;
 
 use sdl2::event::Event;
-use sdl2::EventPump;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 use sdl2::rect::Point;
-use sdl2::render::Canvas;
-use sdl2::video::Window;
-use time::Tm;
-
-fn random_color() -> u8 {
-    rand::random::<u8>()
-}
-
-const WIDTH: usize = 800;
-const HEIGHT: usize = 600;
-
-//fn create_colors() -> Colors { [[0; HEIGHT]; WIDTH] }
-fn create_colors() -> Colors { [0; WIDTH * HEIGHT] }
-
-//type Colors = [[u8; HEIGHT]; WIDTH];
-type Colors = [u8; WIDTH * HEIGHT];
-
-//fn get(colors: &Colors, x: usize, y: usize) -> u8 { colors[x][y] }
-fn get(colors: &Colors, x: usize, y: usize) -> u8 { colors[x + WIDTH * y] }
-
-
-//fn set(colors: &mut Colors, x: usize, y: usize, value: u8) { colors[x][y] = value; }
-fn set(colors: &mut Colors, x: usize, y: usize, value: u8) { colors[x + WIDTH * y] = value; }
-
-fn recalculate_image(mut colors: &mut Colors) {
-    for x in 0..WIDTH - 1 {
-        set(colors, x, HEIGHT - 1, random_color());
-    }
-    for x in 1..WIDTH - 1 {
-        for y in (0..HEIGHT - 1).rev() {
-            let sum = get(colors, x, y) as u16
-                + get(colors, x, y + 1) as u16
-                + get(colors, x - 1, y + 1) as u16
-                + get(colors, x + 1, y + 1) as u16;
-            set(&mut colors, x, y, (sum / 4) as u8);
-        }
-    }
-}
-
-fn redraw_image(colors: &mut Colors, canvas: &mut Canvas<Window>) -> Result<(), String> {
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    canvas.clear();
-    for x in 0..WIDTH - 1 {
-        for y in 0..HEIGHT - 1 {
-            let color = get(colors, x, y);
-            if color < 10 { continue; }
-            canvas.set_draw_color(Color::RGB(color, color, color));
-            canvas.draw_point(Point::new(x as i32, y as i32))?;
-        }
-    }
-    canvas.present();
-    Ok(())
-}
-
-fn update_time(time_info: (Tm, i64, i64)) -> (Tm, i64, i64) {
-    let (prev, sum, num_iterations) = time_info;
-    let now = time::now();
-    let delta_time = now - prev;
-    (now, sum + delta_time.num_milliseconds(), num_iterations + 1)
-}
-
-fn should_quit(pump: &mut EventPump) -> bool {
-    for event in pump.poll_iter() {
-        match event {
-            Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                return true;
-            }
-            _ => {}
-        }
-    }
-    false
-}
-
-fn setup_sdl() -> Result<(Canvas<Window>, EventPump), String> {
-    let sdl = sdl2::init()?;
-    let window = sdl.video()?
-        .window("Fire", WIDTH as u32, HEIGHT as u32)
-        .position_centered()
-        .opengl()
-        .build()
-        .map_err(|e| e.to_string())?;
-    let canvas = window
-        .into_canvas()
-        .build()
-        .map_err(|e| e.to_string())?;
-    let pump = sdl.event_pump()?;
-    Ok((canvas, pump))
-}
+use std::time::Duration;
 
 fn main() -> Result<(), String> {
-    let (mut canvas, mut event_pump) = setup_sdl()?;
-    let mut colors = create_colors();
-    let mut time_info = (time::now(), 0, 0);
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
 
-    loop {
-        if should_quit(&mut event_pump) { break; }
+    let window = video_subsystem.window("SDL2", 640, 480)
+        .position_centered().build().map_err(|e| e.to_string())?;
 
-        recalculate_image(&mut colors);
-        redraw_image(&mut colors, &mut canvas)?;
+    let mut canvas = window.into_canvas()
+        .accelerated().build().map_err(|e| e.to_string())?;
+    let texture_creator = canvas.texture_creator();
 
-        time_info = update_time(time_info);
+    canvas.set_draw_color(sdl2::pixels::Color::RGBA(0, 0, 0, 255));
+
+    let mut timer = sdl_context.timer()?;
+
+    let mut event_pump = sdl_context.event_pump()?;
+
+    // animation sheet and extras are available from
+    // https://opengameart.org/content/a-platformer-in-the-forest
+    let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/characters.bmp"))?;
+    let texture = texture_creator.create_texture_from_surface(&temp_surface)
+        .map_err(|e| e.to_string())?;
+
+    let frames_per_anim = 4;
+    let sprite_tile_size = (32, 32);
+
+    // Baby - walk animation
+    let mut source_rect_0 = Rect::new(0, 0, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_0 = Rect::new(0, 0, sprite_tile_size.0 * 4, sprite_tile_size.0 * 4);
+    dest_rect_0.center_on(Point::new(-64, 120));
+
+    // King - walk animation
+    let mut source_rect_1 = Rect::new(0, 32, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_1 = Rect::new(0, 32, sprite_tile_size.0 * 4, sprite_tile_size.0 * 4);
+    dest_rect_1.center_on(Point::new(0, 240));
+
+    // Soldier - walk animation
+    let mut source_rect_2 = Rect::new(0, 64, sprite_tile_size.0, sprite_tile_size.0);
+    let mut dest_rect_2 = Rect::new(0, 64, sprite_tile_size.0 * 4, sprite_tile_size.0 * 4);
+    dest_rect_2.center_on(Point::new(440, 360));
+
+    let mut walking = true;
+    let mut time = time::now();
+    let mut ticks: i32 = 0;
+
+    let mut speed = 1.0;
+
+    'main: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'main;
+                }
+                Event::KeyDown { keycode: Some(Keycode::Space), .. } => {
+                    walking = !walking;
+                }
+                Event::KeyDown { keycode: Some(Keycode::Up), .. } => {
+                    speed += 0.5;
+                }
+                Event::KeyDown { keycode: Some(Keycode::Down), .. } => {
+                    speed -= 0.5;
+                }
+                _ => {}
+            }
+        }
+
+        let now = time::now();
+        let delta = now - time;
+        time = now;
+
+        if walking {
+            ticks += (delta.num_milliseconds() as f32 * speed) as i32;
+
+            // set the current frame for time
+            source_rect_0.set_x(32 * ((ticks / 100) % frames_per_anim));
+            dest_rect_0.set_x(1 * ((ticks / 14) % 768) - 128);
+
+            source_rect_1.set_x(32 * ((ticks / 100) % frames_per_anim));
+            dest_rect_1.set_x((1 * ((ticks / 12) % 768) - 672) * -1);
+
+            source_rect_2.set_x(32 * ((ticks / 100) % frames_per_anim));
+            dest_rect_2.set_x(1 * ((ticks / 10) % 768) - 128);
+
+            canvas.clear();
+            // copy the frame to the canvas
+            canvas.copy_ex(&texture, Some(source_rect_0), Some(dest_rect_0), 0.0, None, false, false)?;
+            canvas.copy_ex(&texture, Some(source_rect_1), Some(dest_rect_1), 0.0, None, true, false)?;
+            canvas.copy_ex(&texture, Some(source_rect_2), Some(dest_rect_2), 0.0, None, false, false)?;
+            canvas.present();
+        }
     }
 
-    let (_, sum_time, iterations) = time_info;
-    println!("Avg time for frame: {}", sum_time / iterations);
-    println!("Avg FPS:            {}", 1_000 / (sum_time / iterations));
     Ok(())
-}
-
-#[test]
-fn test_add() {
-    let mut ys: [[i32; 4]; 4] = [[0; 4]; 4];
-    println!("{:?}", ys);
-    ys[2][2] = 12;
-    println!("{:?}", ys);
-    println!("{:?}", ys[2][2]);
-    assert_eq!(1 + 2, 3);
 }
