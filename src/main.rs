60e987cbd5db69cc79276b22e4db40d0f281727f
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-04-11 11:06:18 +0200

    Noise

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 extern crate sdl2;
+extern crate rand;
 
+use rand::prelude::*;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::rect::{Rect, Point};
+
+fn random(range: u32) -> u32 {
+    return (rand::random::<f32>() * range as f32 / 2.0 + range as f32 / 2.0) as u32;
+}
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    let width = 800;
+    let height = 600;
 
-    let window = video_subsystem.window("Fire", 800, 600)
+    let window = video_subsystem.window("Fire", width, height)
         .position_centered()
         .opengl()
         .build()
@@ -17,22 +26,27 @@ fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
-    canvas.set_draw_color(Color::RGB(255, 0, 0));
-    canvas.clear();
-    canvas.present();
     let mut event_pump = sdl_context.event_pump()?;
 
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running;
+                }
                 _ => {}
             }
         }
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-        // The rest of the game loop goes here...
+//        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        canvas.set_draw_color(Color::RGB(0, 0, 0));
+        canvas.clear();
+        canvas.set_draw_color(Color::RGB(255, 0, 0));
+        for x in 0..width {
+            for y in 0..random(height / 2) {
+                canvas.draw_point(Point::new(x as i32, y as i32));
+            }
+        }
+        canvas.present();
     }
 
     Ok(())
