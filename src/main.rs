1b2e03694441341d429bb8d9c30cd9371824b4d0
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-04-11 22:00:56 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,20 @@ use sdl2::rect::Rect;
 use sdl2::rect::Point;
 use std::time::Duration;
 
+#[derive(Clone, Copy)]
 enum Direction {
     Forward,
     Backward,
+}
+
+impl Direction {
+    fn get_delta(self) -> Position {
+        let velocity = 3;
+        match self {
+            Direction::Forward => velocity,
+            Direction::Backward => -velocity,
+        }
+    }
 }
 
 type Position = i32;
@@ -35,6 +46,7 @@ impl State {
     }
 
     fn move_by(self, delta: Position) -> State {
+        let delta = self.direction.map_or(0, |d| d.get_delta());
         State { position: self.position + delta, ..self }
     }
 }
@@ -107,7 +119,7 @@ fn main() -> Result<(), String> {
             Some(Direction::Forward) => 1,
             Some(Direction::Backward) => -1,
         };
-        state = state.move_by((delta.num_milliseconds() as f32) as i32 * direction);
+        state = state.move_by((delta.num_milliseconds() as f32) as i32);
 
         source_rect_2.set_x(32 * ((state.position / 100) % frames_per_anim));
         dest_rect_2.set_x(1 * ((state.position / 10) % 768) - 128);
