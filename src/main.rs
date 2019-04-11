9c38f46b26df2875346bc89e797faf0d9f8a5050
Piotr Chmielowski <piotrek.chmielowski@interia.pl>
2019-04-11 14:40:20 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,8 @@ fn redraw_image(colors: &mut [[u8; HEIGHT]; WIDTH], canvas: &mut Canvas<Window>)
     canvas.present();
 }
 
-fn update_time(prev: Tm, sum: i64, num_iterations: i64) -> (Tm, i64, i64) {
+fn update_time(time_info: (Tm, i64, i64)) -> (Tm, i64, i64) {
+    let (prev, sum, num_iterations) = time_info;
     let now = time::now();
     let delta_time = now - prev;
     (now, sum + delta_time.num_milliseconds(), num_iterations + 1)
@@ -68,7 +69,6 @@ fn should_quit(pump: &mut EventPump) -> bool {
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-
     let window = video_subsystem.window("Fire", WIDTH as u32, HEIGHT as u32)
         .position_centered()
         .opengl()
@@ -83,17 +83,13 @@ fn main() -> Result<(), String> {
 
     let mut time_info = (time::now(), 0, 0);
 
-    'running: loop {
-        if should_quit(&mut event_pump) {
-            break 'running;
-        }
+    loop {
+        if should_quit(&mut event_pump) { break; }
 
         recalculate_image(&mut colors);
-
         redraw_image(&mut colors, &mut canvas);
 
-        let (time, sum_time, iterations) = time_info;
-        time_info = update_time(time, sum_time, iterations);
+        time_info = update_time(time_info);
     }
 
     let (_, sum_time, iterations) = time_info;
